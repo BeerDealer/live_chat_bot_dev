57b3d9d7476e59d2cc65d5c8bfd63d8ff384541e
ICockBoy <wasd@ss.ss>
2024-03-15 00:58:14 +0400

    changes

--- user_bot/src/index.ts
+++ user_bot/src/index.ts
@@ -2,8 +2,6 @@ import TelegramBot from "node-telegram-bot-api";
 import { TOKEN } from "./config";
 import { Dispatcher } from "./models/dispathcer";
 
-const test = require("./routers/test");
-
 const bot: TelegramBot = new TelegramBot(TOKEN, {
   polling: true,
 });
